024ee9996a9468bf1690ed9961f5568b6d24f3ef
Sanket Mathur 001888575 <mathurs24@ccs.neu.edu>
2017-10-22 17:23:08 -0400

    1.1

--- CTCI/AllUnique.java
+++ CTCI/AllUnique.java
@@ -2,7 +2,7 @@ import java.util.Set;
 import java.util.HashSet;
 class AllUnique {
 	static public boolean allUnique(String s) {
-		char[] chToStr = s.toCharArray();
+		/*char[] chToStr = s.toCharArray();
 		Set<Character> setChar = new HashSet<Character>();
 		for(int i=0; i<chToStr.length; i++) {
 			setChar.add(chToStr[i]);
@@ -10,9 +10,18 @@ class AllUnique {
 		if(s.length() == setChar.size())
 			return true;
 		else
-			return false;
+			return false;*/
+		boolean[] char_set = new boolean[256];
+		for(int i=0; i<s.length(); i++) {
+			int val = s.charAt(i);
+			System.out.println(val + "   " + char_set[val]);
+			if (char_set[val]) return false;
+			char_set[val] = true;
+		}
+		return true;
 	}
 	public static void main(String[] args) {
-		System.out.println(allUnique ("Sasnket"));
+		//System.out.println(allUnique ("SaSnket"));
+		System.out.println("sanket".charAt(0));
 	}
 }
