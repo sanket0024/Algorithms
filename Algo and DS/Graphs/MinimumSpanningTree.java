a88d3fcfce7d2c46bb3f591e8c2a4d0e077a7b96
Sanket Mathur <sanket0024@gmail.com>
2018-06-02 15:07:12 -0400

    MST

--- Algo and DS/Graphs/MinimumSpanningTree.java
+++ Algo and DS/Graphs/MinimumSpanningTree.java
@@ -2,11 +2,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashMap;
-import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.PriorityQueue;
-import java.util.Set;
 
 /**
  * Miniumum Spanning Tree are formed from a graph
@@ -23,6 +22,8 @@ public class MinimumSpanningTree {
 	
 	/**
 	 * Kruskal MST
+	 * Time complexity: O(E logV)
+	 * Space complexity: O(E)
 	 * @param g
 	 * @return
 	 */
@@ -55,14 +56,20 @@ public class MinimumSpanningTree {
 		return result;
 	}
 	
-	
+	/**
+	 * prim's algorithm
+	 * Time complexity: O(E logV)
+	 * Space complexity:O(V + E)
+	 * Using Priority Queue 
+	 * @param g
+	 * @return
+	 */
 	public List<Edge> prim(Graph g) {
 		List<Edge> result = new ArrayList<>();
-		Set<Vertex> set = new HashSet<>();
+		Vertex[] vts = new Vertex[g.V];
 		Map<Integer, Edge> edgeMap = new HashMap<>();
-		for(int i=0; i<g.E; i++) {
-			edgeMap.put(g.edges[i].src*10+g.edges[i].dest, g.edges[i]);
-		}
+		Map<Integer, Edge> visited = new HashMap<>();
+		populateEdgeMap(g, edgeMap);
 		PriorityQueue<Vertex> pq = new PriorityQueue<>(new Comparator<Vertex>() {
 			public int compare(Vertex v1, Vertex v2) {
 				if(v1.key < v2.key) {
@@ -81,26 +88,55 @@ public class MinimumSpanningTree {
 			else {
 				v.key = Integer.MAX_VALUE;
 			}
-			v.parent = null;
-			pq.offer(v);
+			vts[i] = v;
+			pq.offer(vts[i]);
 		}
+		//printPriorityQueue(pq);
 		while(!pq.isEmpty()) {
-			Vertex v = pq.poll();
-			
+			Vertex u = pq.poll();
+			//System.out.println("pq.poll().key " + u.key + "       pq.poll().val: " + u.val);
+			if(visited.get(u.val) != null) {
+				//System.out.println("Adding " + u.val + " to result : " + visited.get(u.val).src + " to " + visited.get(u.val).dest);
+				result.add(visited.get(u.val));
+			}
+			Iterator<Integer> it = g.adjList[u.val].iterator();
+			while(it.hasNext()) {
+				int v = it.next();
+				//System.out.println(u.val + "  " + v);
+				Edge e = getEdge(u.val, v, edgeMap);
+				if(e!=null && vts[v].key > e.weight) {
+					//System.out.println("removing: " + vts[v].key + "      value: " + vts[v].val);
+					pq.remove(vts[v]);
+					vts[v].key = e.weight;
+					visited.put(v, e);
+					pq.offer(vts[v]);
+				}
+			}
 		}
-		
-		
 		return result;
 	}
 	
-	public void printEdges(List<Edge> edges) {
-		for(int i=0; i<edges.size(); i++) {
-			System.out.println(edges.get(i).src + "->" + edges.get(i).dest);
+	private void populateEdgeMap(Graph g, Map<Integer, Edge> edgeMap) {
+		for(int i=0; i<g.E; i++) {
+			edgeMap.put(g.edges[i].src*10 + g.edges[i].dest, g.edges[i]);
+			edgeMap.put(g.edges[i].dest*10 + g.edges[i].src, g.edges[i]);
 		}
 	}
 	
+	private Edge getEdge(int src, int dest, Map<Integer, Edge> edgeMap) {
+		return (edgeMap.get(src*10 + dest));
+	}
+	
+	public void printEdges(List<Edge> edges) {
+		System.out.println(edges.size());
+		for(int i=0; i<edges.size(); i++) {
+			System.out.println(edges.get(i).src + "->" + edges.get(i).dest);
+		}
+		System.out.println();
+	}
+	
 	public static void main(String[] args) {
-		Graph g = new Graph(8, 9);
+		Graph g = new Graph(6, 8);
 		g.addUndirectedEdge(0, 1, 4);
 		g.addUndirectedEdge(0, 5, 1);
 		g.addUndirectedEdge(1, 2, 2);
@@ -109,11 +145,14 @@ public class MinimumSpanningTree {
 		g.addUndirectedEdge(2, 3, 8);
 		g.addUndirectedEdge(3, 4, 8);
 		g.addUndirectedEdge(4, 5, 7);
-		g.addUndirectedEdge(7, 6, 9);
 		
 		MinimumSpanningTree mst = new MinimumSpanningTree();
+		System.out.println("**********Kruskal**********");
 		List<Edge> kru = mst.kruskal(g);
 		mst.printEdges(kru);
+		
+		System.out.println("**********Prim**********");
+		List<Edge> pri = mst.prim(g);
+		mst.printEdges(pri);
 	}
-	
 }
--- Algo and DS/Graphs/Vertex.java
+++ Algo and DS/Graphs/Vertex.java
@@ -9,7 +9,6 @@
 public class Vertex {
 
 	public int key;
-	public Vertex parent;
 	public int val;
 	
 	/**
@@ -25,7 +24,6 @@ public class Vertex {
 	 */
 	public Vertex(int val) {
 		this.val = val;
-		parent = null;
 		key = 0;
 	}
 }
